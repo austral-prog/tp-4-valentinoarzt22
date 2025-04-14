d11d013f4875f6f80ed7e659bb6e422886493b1e
valentinoarzt22 <vrarzt@mail.austral.edu.ar>
2025-04-14 12:04:30 -0300

    Update line.py

--- line.py
+++ line.py
@@ -8,17 +8,10 @@ def line():
     y1 = cofA * cofX1 + cofB
     y2 = cofA * cofX2 + cofB
     distancia = math.sqrt((cofX2 - cofX1) ** 2 + (y2 - y1) ** 2)
-    print(f"""El coeficiente A de su ecuación de la recta es: {cofA}
-    El coeficiente B de su ecuación de la recta es: {cofB}
-    El coeficiente X1 de su ecuación de la recta es: {cofX1}
-    El coeficiente X2 de su ecuación de la recta es: {cofX2}
-    """)
-    
-    print(f"""Para la siguiente ecuación:
-    Y = {cofA} + {cofB}
-    
-    Dado los siguientes puntos
-           P1 ({cofX1}, {y1})
-           P2 ({cofX2}, {y2})
-           
-    La distancia entre ellos es: {distancia}""")
+    print(f'El coeficiente A de su ecuación de la recta es: {cofA}')
+    print(f'El coeficiente B de su ecuación de la recta es: {cofB}')
+    print(f'El coeficiente X1 de su ecuación de la recta es: {cofX1}')
+    print(f'El coeficiente X2 de su ecuación de la recta es: {cofX2}\n')
+    print(f'Para la siguiente ecuación:\n\tY = {cofA}X + {cofB}\n')
+    print(f'Dados los siguientes puntos:\n\tP1 ({cofX1}, {y1})\n\tP2 ({cofX2}, {y2})\n')
+    print(f'La distancia entre ellos es: {distancia}')
